0d772ff848ac178100190f42b444a605cc8cc471
ryand67 <rwdevenney@gmail.com>
2021-07-16 20:13:10 -0400

    list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import bookList from './util/BookList';
+import Card from './util/Card';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  console.log(bookList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/util/Book.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(title, author, genre, amazon) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.amazon = amazon;
+    }
+}
+
+export default Book;
